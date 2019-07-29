dbac17713902ae10748712798bcd1bdfd47f8245
Wesley Arrington <cdsigma271@gmail.com>
2019-07-29 13:50:07 -0700

    updating campaign edit syle

--- frontend/components/campaign_edit/side_bar.jsx
+++ frontend/components/campaign_edit/side_bar.jsx
@@ -5,6 +5,12 @@ export default class SideBar extends Component {
         return (
             <div className="campaign-edit-side-bar">
                 <h1>Side Bar</h1>
+                <select>
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
             </div>
         )
     }
